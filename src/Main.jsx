cf638367624d64111077584a98482f7168d73413
Henrique O. <lemonheadch@outlook.com>
2025-10-05 18:48:06 -0300

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Your Tailwind CSS file
+import './index.css'; //this file garantees the application will route through App.jsx and index.scc before the main App.jsx
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
